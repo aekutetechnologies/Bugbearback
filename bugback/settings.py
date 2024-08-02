d99aed909f53b7117da9e05a6deb4600aa92c23d
smruti <aekutetechnologies@gmail.com>
2024-08-02 19:17:13 +0530

    SOlve profile pic image url issue

--- bugback/settings.py
+++ bugback/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*", "bugbearback.onrender.com"]
 
-WEB_URL = "https://bugbearback.onrender.com/"
+WEB_URL = "https://bugbearback.onrender.com"
 # Application definition
 
 INSTALLED_APPS = [
